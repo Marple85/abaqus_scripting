eb7730c13fa61080bf821500d816b472361ab11a
Martin Pletz <martin.pletz@unileoben.ac.at>
2020-09-10 10:04:59 +0200

    added python file

--- inp_file/run_inp_models_eval.py
+++ inp_file/run_inp_models_eval.py
@@ -5,6 +5,22 @@ from subprocess import call
 # open input file and split string into list of strings
 with open('pull_plate.inp','r') as f:
     lines = f.read().split('\n')
+
+def evaluate_rf2(dat_file):
+    # open the dat file with results in it
+    with open(dat_file+'.dat','r') as f:
+        lines = f.read().split('\n')
+    # take the lines with reference point results
+    rp_lines = []
+    for line in lines:
+        if line.startswith('         91 '):
+            rp_lines += [line]
+    # result for last frame
+    res_line = rp_lines[-1]
+    # split ste string and delete empty strings from list
+    res_line = [i for i in res_line.split(' ') if i != '']
+    # take the second from last string (RF2) and convert it to a float
+    return float(res_line[-2])
     
 def make_inp_file(lines,string,value):
     # copy the original lines
@@ -20,14 +36,21 @@ def make_inp_file(lines,string,value):
         f.write('\n'.join(lines_temp))
     # run the inp file (subprocess.call)
     call('abaqus interactive job='+inp_name, shell=True)
-    return
+    # read from the dat-file
+    rf2 = evaluate_rf2(inp_name)
+    print('Model with {} = {} MPa: rf2 = {} N'.format(string,value,rf2))
+    return rf2
 
 # call the function once
-make_inp_file(lines,'sig_yield',380.)
+#make_inp_file(lines,'sig_yield',380.)
 
 # create list of sy values
 sy_list = [500., 750., 1000., 1250., 1500.]
 
+result_dict = {}
+
 # run the inp-files for each sy value
 for sy in sy_list:
-    make_inp_file(lines,'sig_yield',sy)
+    result_dict[sy] = make_inp_file(lines,'sig_yield',sy)
+
+print(result_dict)
